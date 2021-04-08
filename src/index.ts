5c3b4a085fe500a2a76fa8a3703bacf56157e558
Jonathan <jtellkampf@gmail.com>
2021-04-08 23:59:38 +0100

    Created Characters collection class to help sorter class sort strings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
@@ -5,3 +6,8 @@ const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const secondSort = new Sorter(charactersCollection);
+secondSort.sort();
+console.log(charactersCollection.data);
