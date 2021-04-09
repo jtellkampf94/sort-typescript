dd30779f08e333771fb0dcb9cfecabc09ad844c9
Jonathan <jtellkampf@gmail.com>
2021-04-09 01:22:28 +0100

    Refactored sorter class to abstract class, made linked list charactercollection and numberscollection inherit from sorter class to enable sorting functionality

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { LinkedList } from "./LinkedList";
 import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
-sorter.collection = charactersCollection;
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +15,5 @@ linkedList.add(500);
 linkedList.add(-3);
 linkedList.add(-10);
 linkedList.add(4);
-sorter.collection = linkedList;
-sorter.sort();
+linkedList.sort();
 linkedList.print();
